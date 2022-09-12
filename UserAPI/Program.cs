a2343f76d59e3d154b009fcca7ced933a5954a4d
George Hill <george.fxmediaweb@gmail.com>
2022-09-12 16:38:20 +0800

    update health check UI

--- UserAPI/Program.cs
+++ UserAPI/Program.cs
@@ -1,12 +1,26 @@
+using HealthChecks.System;
+using HealthChecks.UI.Client;
+using HealthChecks.UI.Configuration;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using UserAPI.Data;
 using UserAPI.Services;
 using UserAPI.Services.Interfaces;
+using UserAPI.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddDbContext<DbContextClass>();
+builder.Services.AddHealthChecks()
+    .AddSqlServer(StaticConfigurationManager.AppSetting["ConnectionStrings:DefaultConnection"])
+    .AddDiskStorageHealthCheck(delegate (DiskStorageOptions diskStorageOptions)
+    {
+        diskStorageOptions.AddDrive(@"C:\", minimumFreeMegabytes: 5000);
+    }, name: "C Drive", HealthStatus.Degraded);
+
+builder.Services.AddHealthChecksUI().AddInMemoryStorage();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,6 +35,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseHealthChecks("/hc", new HealthCheckOptions()
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+app.UseHealthChecksUI(delegate (Options options)
+{
+    options.UIPath = "/hc-ui";
+});
 
 app.UseHttpsRedirection();
 
