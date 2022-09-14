af18e9ecc27e87ebd2ea4b82fda7c3ef4ed01bad
George Hill <george.fxmediaweb@gmail.com>
2022-09-14 09:19:13 +0800

    product API microservice

---
+++ ProductAPI/Data/DBContextClass.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductAPI.Models;
+
+namespace ProductAPI.Data
+{
+    public class DBContextClass : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DBContextClass(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
